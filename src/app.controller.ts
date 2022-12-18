c3311cd324a6fb3a96ece0cb76062581b48ce6b1
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-12-18 15:16:18 -0300

    feat: criação da rota de criação com validação dos campos

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { randomUUID } from 'node:crypto';
+import { CreateNotificationBody } from './create.notification-body';
 
 @Controller('notifications')
 export class AppController {
@@ -12,13 +13,15 @@ export class AppController {
   }
 
   @Post()
-  async create() {
+  async create(@Body() body: CreateNotificationBody) {
+    const { content, category, recipientId } = body;
+
     await this.prima.notification.create({
       data: {
         id: randomUUID(),
-        content: 'Nova solicitação de amizada',
-        category: 'social',
-        recipientId: randomUUID(),
+        content,
+        category,
+        recipientId,
       },
     });
   }
