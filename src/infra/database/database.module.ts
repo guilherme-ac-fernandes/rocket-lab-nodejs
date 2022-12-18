fa1789cb64763fdefa90e5c6371e608b57332c11
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-12-18 18:52:58 -0300

    feat: organização da aplicação + estruturação do acoplamento com o banco de dados

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { NotificationsRepository } from '../../application/repositories/notifications-repository';
+import { PrismaNotificationsRepository } from './prisma/repositories/prisma-notifications-repository';
+// import { NotificationsRepository } from '@application/repositories/notifications-repository';
+// import { PrismaNotificationsRepository } from '@infra/database/prisma/repositories/prisma-notifications-repository';
+
+import { PrismaService } from './prisma/prisma.service';
+
+@Module({
+  providers: [
+    PrismaService,
+    {
+      provide: NotificationsRepository,
+      useClass: PrismaNotificationsRepository,
+    },
+  ],
+  exports: [NotificationsRepository],
+})
+export class DatabaseModule {}
